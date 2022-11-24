57a43abce623b541c1b3ba18623c24b1a4c2a4bd
nasanchez7 <nadirblanco02@gmail.com>
2022-11-24 20:23:10 -0300

    trasladando fs a base de datos

--- app.js
+++ app.js
@@ -1,141 +1,6 @@
-const fs = require('fs');
-
-class Contenedor{
-    constructor(archivo, id ){
-        this.archivo = `./${archivo}.txt`
-        this.id = 1
-    }
-
-    async save(objeto){
-        try{
-            if(!fs.existsSync(this.archivo)) {
-                await fs.promises.writeFile(this.archivo, JSON.stringify([
-                    {
-                        id: this.id,
-                        ...objeto
-                    }
-                ]));
-                return `Has agregado ${objeto.title} con el id ${this.id}`
-            }else{
-                const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-                const json = JSON.parse(archivo);
-                if(json.length > 0){
-                    json.push({
-                        id: json.length + 1,
-                        ...objeto
-                    })
-                    await fs.promises.writeFile(this.archivo, JSON.stringify(json))
-                    return { msj: `Has agregado ${objeto.title} con el id ${json.length}`}
-                }
-            }
-        }
-        catch(e){
-            console.log(`Ha habido un error al leer el archivo ${this.archivo}`)
-        }
-    }
-
-    async getById(id){
-        try {
-            const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-            const json = JSON.parse(archivo);
-            if (json.length > 0) {
-                const obj = json.find(obj => obj.id === id)
-                if (obj){
-                    return obj
-                }else{
-                    return {error: "producto no encontrado"}
-                } 
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async getAll(){
-        try {
-            const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-            const json = JSON.parse(archivo);
-            if (json.length > 0) {
-                return json
-            }
-            console.log("Archivo vacio")
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async deleteById(id){
-        try {
-            const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-            const json = JSON.parse(archivo);
-            if (json.length > 0) {
-                const index = json.findIndex(obj => obj.id === id)
-                if (index === -1) {
-                    console.log(`El objeto no existe`)
-                } else {
-                    json.splice(index, 1)
-                    await fs.promises.writeFile(this.archivo, JSON.stringify(json))
-                }
-            }
-        }catch(error){
-            console.log(error)
-        } 
-    } 
-    async deleteAll(){
-        try {
-            await fs.promises.writeFile(this.archivo, "[]")
-        } catch (error) {
-            console.log(error)
-        }
-    }
-}
-
-class Mensajes {
-    constructor(archivo){
-        this.archivo = `./${archivo}.txt`
-    }
-
-    async save(objeto){
-        try{
-            if(!fs.existsSync(this.archivo)) {
-                await fs.promises.writeFile(this.archivo, JSON.stringify([
-                    {
-                        ...objeto
-                    }
-                ]));
-            }else{
-                const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-                const json = JSON.parse(archivo);
-                if(json.length > 0){
-                    json.push({
-                        ...objeto
-                    })
-                    await fs.promises.writeFile(this.archivo, JSON.stringify(json))
-                }
-            }
-        }
-        catch(e){
-            console.log(`Ha habido un error al leer el archivo ${this.archivo}`)
-        }
-    }
-
-    async getAll(){
-        try {
-            const archivo = await fs.promises.readFile(this.archivo, 'utf-8')
-            const json = JSON.parse(archivo);
-            if (json.length > 0) {
-                return json
-            }
-            console.log("Archivo vacio")
-        } catch (error) {
-            console.log(error)
-        }
-    }
-}
-
-const archivo = new Contenedor("productos");
-const mensajes = new Mensajes("mensajes");
-
+const Contenedor = require("./contenedores/contenedorSql.js")
+const Mensajes = require('./contenedores/contenedorSqlite3.js')
+const options = require("./connection/options.js")
 const express = require('express')
 const { Server: HttpServer } = require('http')
 const { Server: IOServer } = require('socket.io')
@@ -143,6 +8,9 @@ const { Server: IOServer } = require('socket.io')
 const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
+
+const archivo = new Contenedor(options.mysql, "ecommerce");
+const mensajes = new Mensajes(options.sqlite3, "mensajes");
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
