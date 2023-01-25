17e4ece79cf6f105fe6aa0c82254ee609d7800b9
nasanchez7 <nadirblanco02@gmail.com>
2023-01-25 19:01:59 -0300

    agregando clusters

--- app-no-bloqueante.js
+++ app-no-bloqueante.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { fork } = require('child_process');
 const app = express();
+const cluster = require('cluster')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,36 +15,84 @@ const argv = parseArgs(process.argv.slice(2))
 //Puerto
 const PORT =  Number(argv.port) || 8080
 
-app.get("/", (req, res) => {
-    res.send({
-        Ruta: "/"
+//Modo de ejecucion
+const mode = argv.mode
+
+//Procesadores
+const numCPUs = require('os').cpus().length
+
+if(mode === 'fork' || mode === undefined){
+    app.get("/", (req, res) => {
+        res.send({
+            Ruta: "/",
+            puerto: PORT
+        })
     })
-})
-
-app.get("/info", (req, res) => {
-    res.render("info", {info: [
-        {clave: "Argumentos de entrada", valor: process.argv.slice(2)},
-        {clave: "Sistema operativo", valor: process.platform},
-        {clave: "Version de node", valor: process.version},
-        {clave: "Memoria total reservada", valor: process.memoryUsage().rss},
-        {clave: "Path de ejecucion", valor: "5"},
-        {clave: "Process id", valor: process.pid},
-        {clave: "Carpeta del proyecto", valor: process.cwd()},
-    ]})
-})
-
-app.get("/api/randoms", (req, res) => {
-
-    const cantidadNum = req.query.cant ? req.query.cant : 100000000
-    const computo = fork('./scripts/CalcularRandom.js')
-    computo.send(cantidadNum)
-    computo.on('message',(sum) => {
-        console.log(sum) 
-        res.json(sum)
+    app.get("/info", (req, res) => {
+        res.render("info", {info: [
+            {clave: "Argumentos de entrada", valor: process.argv.slice(2)},
+            {clave: "Sistema operativo", valor: process.platform},
+            {clave: "Version de node", valor: process.version},
+            {clave: "Memoria total reservada", valor: process.memoryUsage().rss},
+            {clave: "Path de ejecucion", valor: "5"},
+            {clave: "Process id", valor: process.pid},
+            {clave: "Carpeta del proyecto", valor: process.cwd()},
+            {clave: "Numero de procesadores", valor: numCPUs}
+        ]})
     })
-    
-})
+    app.get("/api/randoms", (req, res) => {
+        console.log(PORT)
+        const cantidadNum = req.query.cant ? req.query.cant : 100000000
+        const computo = fork('./scripts/CalcularRandom.js')
+        computo.send(cantidadNum)
+        computo.on('message',(sum) => {
+            res.json(sum)
+        })
 
-const server = app.listen(PORT, ()=>{
-    console.log("Server escuchando en puerto " + PORT)
-})
+    })
+    const server = app.listen(PORT, ()=>{
+        console.log("Server escuchando en puerto " + PORT)
+    })
+}
+if(mode === 'cluster'){
+    if(cluster.isPrimary){
+        console.log("Worker master escuchando con " + numCPUs + " procesadores")
+        for (let index = 0; index < numCPUs; index++) {
+            cluster.fork()
+        }
+        cluster.on('exit', (worker, code, signal)=>{
+            console.log('Worker exit')
+        })
+    }else{
+        app.get("/", (req, res) => {
+            res.send({
+                Ruta: "/",
+                puerto: PORT
+            })
+        })
+        app.get("/info", (req, res) => {
+            res.render("info", {info: [
+                {clave: "Argumentos de entrada", valor: process.argv.slice(2)},
+                {clave: "Sistema operativo", valor: process.platform},
+                {clave: "Version de node", valor: process.version},
+                {clave: "Memoria total reservada", valor: process.memoryUsage().rss},
+                {clave: "Path de ejecucion", valor: "5"},
+                {clave: "Process id", valor: process.pid},
+                {clave: "Carpeta del proyecto", valor: process.cwd()},
+                {clave: "Numero de procesadores", valor: numCPUs}
+            ]})
+        })
+        app.get("/api/randoms", (req, res) => {
+            const cantidadNum = req.query.cant ? req.query.cant : 100000000
+            const computo = fork('./scripts/CalcularRandom.js')
+            computo.send(cantidadNum)
+            computo.on('message',(sum) => {
+                res.json(sum)
+            })
+        })
+        const server = app.listen(PORT, ()=>{
+            console.log("Worker escuchando en puerto " + PORT + " con pdi: " + process.pid)
+        })
+    }
+}
+
